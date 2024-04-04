56151583f8b85479e3eb3e50c36bc7efa96c82fe
DNCCsuport <161918377+DNCCsuport@users.noreply.github.com>
2024-04-04 15:56:37 +0530

    Add files via upload

---
+++ zapapi/model/watireplay.cs
@@ -0,0 +1,15 @@
+﻿namespace zapapi.model
+{
+    public class watireplay
+    {
+        public string name { get; set; }
+        public string clinicloction { get; set; }
+        public string time { get; set; } 
+        public string Date { get; set; }
+        public string email { get; set; }
+        public string callingnumber { get; set; }
+        public string day { get; set; }
+        public string phone { get; set; }
+
+    }
+}
